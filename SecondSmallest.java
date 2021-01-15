7bb87d463ef921876907289a3b6897a79083d1e6
Ananya Priyadarshini <priyadarshiniananya776@gmail.com>
2021-01-15 19:40:16 +0530

    Lab 7 - Exercise 5 Added

---
+++ SecondSmallest.java
@@ -0,0 +1,17 @@
+import java.util.*;  
+import java.util.Collections;
+
+public class SecondSmallest{  
+	public static void main(String args[]){  
+	       Integer array[]={1,2,5,6,3,7};  
+	        System.out.println("Second Smallest: "+getSecondSmallest(array,6));  
+	     
+	}
+public static int getSecondSmallest(Integer[] array, int total){  
+      List<Integer> list=Arrays.asList(array);  
+      Collections.sort(list);  
+     int element=list.get(1);  
+     return element;  
+}  
+
+}
